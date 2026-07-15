74a41518d335b60ed6ec9bd353184b25355d1f28
Vikram Kamat <vikramkamat2004@gmail.com>
2026-07-14 21:00:02 -0400

    working through 3.1

--- Math2001/03_Parity_and_Divisibility/01_Parity.lean
+++ Math2001/03_Parity_and_Divisibility/01_Parity.lean
@@ -103,16 +103,36 @@ example {m n : ℤ} (hm : Odd m) (hn : Even n) : Odd (n + m) := by
     _ = 2 * (b + a) + 1 := by ring
 
 example {p q : ℤ} (hp : Odd p) (hq : Even q) : Odd (p - q - 4) := by
-  sorry
+  obtain ⟨a, ha⟩ := hp
+  obtain ⟨b, hb⟩ := hq
+  use a - b - 2
+  calc
+    p - q - 4 = 2 * a + 1 - (2 * b) - 4 := by rw[ha,hb]
+    _ = 2 * (a - b - 2) + 1 := by ring
 
 example {a b : ℤ} (ha : Even a) (hb : Odd b) : Even (3 * a + b - 3) := by
-  sorry
+  obtain ⟨x, hx⟩ := ha
+  obtain ⟨y, hy⟩ := hb
+  use 3 * x + y -1
+  calc
+    3 * a + b - 3 = 3 * (2 * x) + (2 * y + 1) - 3 := by rw[hx,hy]
+    _ = 2 * (3 * x + y - 1) := by ring
 
 example {r s : ℤ} (hr : Odd r) (hs : Odd s) : Even (3 * r - 5 * s) := by
-  sorry
+  obtain ⟨a, ha⟩ := hr
+  obtain ⟨b, hb⟩ := hs
+  use 3 * a - 5 * b - 1
+  calc
+    3 * r - 5 * s = 3 * (2 * a + 1) - 5 * (2 * b + 1) := by rw[ha,hb]
+    _ = 2 * (3 * a - 5 * b - 1) := by ring
 
 example {x : ℤ} (hx : Odd x) : Odd (x ^ 3) := by
-  sorry
+  obtain ⟨a, ha⟩ := hx
+  use 4 * a ^ 3 + 6 * a ^ 2 + 3 * a
+  calc
+    x ^ 3 = (2 * a + 1) ^ 3 := by rw[ha]
+    _ = 8 * a ^ 3 + 12 * a ^ 2 + 6 * a + 1 := by ring
+    _ = 2 * (4 * a ^ 3 + 6 * a ^ 2 + 3 * a) + 1 := by ring
 
 example {n : ℤ} (hn : Odd n) : Even (n ^ 2 - 3 * n + 2) := by
   sorry
